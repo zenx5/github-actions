af8edb255301319305822c564c275bd8a68136ec
Octavio Martinez <omartinez1618@gmail.com>
2023-08-19 08:52:18 -0400

    parse Input

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const input = document.querySelector('#number')
 
 document.querySelector('#up').addEventListener('click', ()=>{
-    input.value += 1
+    input.value = parseFloat(input.value) + 1
 })
 
 document.querySelector('#down').addEventListener('click', ()=>{
-    input.value -= 1
+    input.value = parseFloat(input.value) - 1
 })
